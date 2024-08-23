dd246a49cf86c7f7718c01bf65ebae9fcb6edcc3
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-08-23 16:56:15 -0300

    style: refactor App component structure for better readability and alignment

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ function App() {
   }
 
   return (
-    <main className="flex flex-1 items-center justify-center min-h-screen bg-primary flex-col relative pb-4">
-      <h1 className="font-bold text-6xl text-secondary">Spot Stats</h1>
-      <AudioLines strokeWidth={3} size={40} color="white" className="mt-2" />
-      <span className="font-semibold text-md text-white mt-2">
+    <main className="flex items-center justify-center min-h-screen bg-primary flex-col relative pb-4">
+      <div className="flex flex-row items-center justify-center gap-2">
+        <AudioLines strokeWidth={3} size={60} color="white" className="mt-2" />
+        <h1 className="font-bold text-6xl text-secondary">Spot Stats</h1>
+      </div>
+      <span className="font-semibold text-md text-white">
         Get your statistics from Spotify
       </span>
       <button
-        className="bg-secondary text-white rounded-md py-2 px-4 font-bold mt-2 hover:bg-secondary-hover"
+        className="bg-secondary text-white rounded-md py-2 px-4 font-bold mt-4 hover:bg-secondary-hover"
         onClick={handleLogin}
       >
         Login with Spotify
