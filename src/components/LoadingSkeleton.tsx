8743408a9d5bb9c26584d4b4e4dee68869400610
Maicon Friedel <maicon.friedel@gmail.com>
2024-08-24 21:42:21 -0300

    style: changed skeleton loading

--- src/components/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton.tsx
@@ -8,7 +8,13 @@ export default function LoadingSkeleton({ quantity }: LoadingSkeletonProps) {
   return (
     <>
       {Array.from({ length: quantity }).map((_, index) => (
-        <Skeleton key={index} className="h-[100px] rounded-xl" />
+        <div key={index} className="flex items-center space-x-4">
+          <Skeleton key={index} className="w-16 h-16 rounded-lg" />
+          <div>
+            <Skeleton className="w-32 h-5 rounded-none" />
+            <Skeleton className="w-32 h-3 rounded-none mt-2" />
+          </div>
+        </div>
       ))}
     </>
   );
